94b4743f1fc5ef2236ec5e15684daca65516a287
Ayoya22 <djoukamsimplice@gmail.com>
2020-04-21 11:35:16 +0100

    Created the course class
    Signed-off-by: Ayoya22 <djoukamsimplice@gmail.com>

---
+++ src/main/java/com/in28minutes/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/course/Course.java
@@ -0,0 +1,11 @@
+package com.in28minutes.fullstack.springboot.maven.crud.springbootfullstackcrudfullstackwithmaven.course;
+
+public class Course {
+    private Long id;
+    private String username;
+    private String description;
+    //no arg constructor
+    //constructor with 3 args
+    //getters and setters
+    //hashcode and equals
+}
