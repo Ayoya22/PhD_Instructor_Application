ddbf0102410be455625f5b128d3dbfaa4b20dd03
Ayoya22 <djoukamsimplice@gmail.com>
2020-04-23 13:09:22 +0100

    Added getCourse method
    Signed-off-by: Ayoya22 <djoukamsimplice@gmail.com>

--- src/main/java/com/in28minutes/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/course/CourseResource.java
+++ src/main/java/com/in28minutes/fullstack/springboot/maven/crud/springbootfullstackcrudfullstackwithmaven/course/CourseResource.java
@@ -1,10 +1,8 @@
 package com.in28minutes.fullstack.springboot.maven.crud.springbootfullstackcrudfullstackwithmaven.course;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,22 @@ public class CourseResource {
     @GetMapping("/instructors/{username}/courses")
     public List<Course> getAllCourses(@PathVariable String username) {
         return courseManagementService.findAll();
+    }
+
+    @DeleteMapping("/instructors/{username}/courses/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable String username, @PathVariable long id) {
+
+        Course course = courseManagementService.deleteById(id);
+
+        if (course != null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/instructors/{username}/courses/{id}")
+    public Course getCourse(@PathVariable String username, @PathVariable long id) {
+        return courseManagementService.findById(id);
     }
 }
